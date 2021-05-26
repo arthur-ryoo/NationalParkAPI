857058f6da0dd2cfd5b44a155345e4e60dca8e21
Arthur Ryoo <jene0523@gmail.com>
2021-05-25 21:34:56 -0700

    Add repository to services

--- ParkyAPI/Startup.cs
+++ ParkyAPI/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using ParkyAPI.Data;
+using ParkyAPI.Repository;
+using ParkyAPI.Repository.IRepository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -30,7 +32,10 @@ namespace ParkyAPI
             // Add Db Context
             services.AddDbContext<ApplicationDbContext>
                 (options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-            
+
+            // Add repository
+            services.AddScoped<INationalParkRepository, NationalParkRepository>();
+
             services.AddControllers();
         }
 
