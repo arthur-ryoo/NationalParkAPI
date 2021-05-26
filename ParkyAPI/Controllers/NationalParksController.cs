f80a90fa2f148afee66b361d6fceb8bf76af4270
Arthur Ryoo <jene0523@gmail.com>
2021-05-25 22:45:17 -0700

    Create get requests

---
+++ ParkyAPI/Controllers/NationalParksController.cs
@@ -0,0 +1,56 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ParkyAPI.Models.Dtos;
+using ParkyAPI.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NationalParksController : Controller
+    {
+        private INationalParkRepository _npRepo;
+        private readonly IMapper _mapper;
+
+        // dependency injection to use repository
+        public NationalParksController(INationalParkRepository npRepo, IMapper mapper)
+        {
+            _npRepo = npRepo;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult GetNationalParks()
+        {
+            var objList = _npRepo.GetNationalParks();
+
+            var objDto = new List<NationalParkDto>();
+
+            foreach(var obj in objList)
+            {
+                objDto.Add(_mapper.Map<NationalParkDto>(obj));
+            }
+            return Ok(objDto);
+        }
+
+        [HttpGet("{nationalParkId:int}")]
+        public IActionResult GetNationalPark(int nationalParkId)
+        {
+            var obj = _npRepo.GetNationalPark(nationalParkId);
+            
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            var objDto = _mapper.Map<NationalParkDto>(obj);
+            return Ok(objDto);
+
+        }
+    }
+}
