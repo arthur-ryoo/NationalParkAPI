a52b8d4bbbb6927e2d5fa103719a398cc279e7df
Arthur Ryoo <jene0523@gmail.com>
2021-05-31 23:16:19 -0700

    Add comments

--- ParkyWeb/SD.cs
+++ ParkyWeb/SD.cs
@@ -5,10 +5,12 @@ using System.Threading.Tasks;
 
 namespace ParkyWeb
 {
-    public class SD
+    // static details
+    public static class SD
     {
         public static string APIBaseUrl = "https://localhost:44384/";
-        public static string NationalParkAPIPath = APIBaseUrl + "api/v1/nationalparks";
-        public static string TrailAPIPath = APIBaseUrl + "api/v1/trails";
+        public static string NationalParkAPIPath = APIBaseUrl + "api/v1/nationalparks/";
+        public static string TrailAPIPath = APIBaseUrl + "api/v1/trails/";
+
     }
 }
