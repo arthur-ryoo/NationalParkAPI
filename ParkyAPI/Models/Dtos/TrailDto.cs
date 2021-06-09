58b54eab6a9491c01ccf721931a82d32c292a4d0
Arthur Ryoo <jene0523@gmail.com>
2021-06-08 21:30:41 -0700

    Add elevation property

--- ParkyAPI/Models/Dtos/TrailDto.cs
+++ ParkyAPI/Models/Dtos/TrailDto.cs
@@ -22,5 +22,8 @@ namespace ParkyAPI.Models.Dtos
         public int NationalParkId { get; set; }
 
         public NationalParkDto NationalPark { get; set; }
+
+        [Required]
+        public double Elevation { get; set; }
     }
 }
